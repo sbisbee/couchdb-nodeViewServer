f1117723f6f0f3a3e0038f6c4cc3d0ed21f180bf
Jan Lehnardt <jan@apache.org>
2012-11-14 18:12:37 +0100

    fix buffering issue, add debug()

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ var vm = require('vm');
 var map = require('./map.js');
 var reduce = require('./reduce');
 
+var fs = require("fs");
+
+function debug(m) {
+  fs.appendFileSync("/tmp/l.log", "\n");
+  fs.appendFileSync("/tmp/l.log", JSON.stringify(m));
+  fs.appendFileSync("/tmp/l.log", "\n");
+}
+
 var ERROR = {
   MAP_NOT_A_FUNC: { error: 100, reason: "Your map function does not evaluate to a JS function." }
 };
@@ -21,78 +29,99 @@ var couch = {
 };
 
 process.on('uncaughtException', function(err) {
-  couch.log('Uncaught Exception: %s', err);
+  couch.log('Uncaught Exception:', JSON.stringify(err));
 });
 
 process.stdin.setEncoding('utf-8');
 
-process.stdin.on('data', function(line) {
+var lineInBuf = '';
+
+process.stdin.on('data', function(lineIn) {
   var evaled;
 
-  if(!line || line === '\n') {
+  if(!lineIn || lineIn === '\n') {
     return;
   }
 
-  try {
-    line = JSON.parse(line);
-  }
-  catch(e) {
-    couch.log('Invalid JSON sent.');
-    couch.sendLine(false);
-
+  lineInBuf += lineIn;
+  if(!lineInBuf.search("\n")) {
+    // not a full line yet, keep reading.
     return;
   }
 
-  if(!line[0]) {
-    couch.log('Expected an array, got something else.');
-    couch.sendLine(false);
-
-    return;
-  }
-
-  switch(line[0]) {
-    case 'reset':
-      map.reset();
-
-      break;
-
-    case 'add_fun':
-      try {
-        map.addFunction(line[1]);
-      }
-      catch(e) {
-        couch.log(e);
-        couch.sendLine(ERROR.MAP_NOT_A_FUNC);
-        return;
-      }
-
-      break;
-
-    case 'map_doc':
-      couch.sendLine(map.runDoc(line[1]));
+  // we do have at least one line!
+  var lines = lineInBuf.split("\n");
+  lineInBuf = '';
+  lines.forEach(function(lineRaw) {
+    if(lineRaw == "" || !lineRaw.search("\n")) {
+      // last line in our lines,
+      // not full, back to the buffer
+      lineInBuf = lineRaw;
       return;
+    }
 
-      break;
-
-    case 'reduce':
-      couch.sendLine([ true, reduce.reduce(line[1], line[2]) ]);
-      return;
-
-      break;
-
-    case 'rereduce':
-      couch.sendLine([ true, reduce.rereduce(line[1], line[2]) ]);
-      return;
-
-    default:
-      couch.log('Not implemented yet.');
+    try {
+      line = JSON.parse(lineRaw);
+    }
+    catch(e) {
+      // couch.log('Invalid JSON sent.');
+      debug('Invalid JSON sent.');
       couch.sendLine(false);
-      return;
-      
-      break;
-  }
 
-  couch.sendLine(true);
+      return;
+    }
+
+    if(!line[0]) {
+      couch.log('Expected an array, got something else.');
+      couch.sendLine(false);
+
+      return;
+    }
+
+    switch(line[0]) {
+      case 'reset':
+        map.reset();
+
+        break;
+
+      case 'add_fun':
+        try {
+          map.addFunction(line[1]);
+        }
+        catch(e) {
+          couch.log(e);
+          couch.sendLine(ERROR.MAP_NOT_A_FUNC);
+          return;
+        }
+
+        break;
+
+      case 'map_doc':
+        couch.sendLine(map.runDoc(line[1]));
+        return;
+
+        break;
+
+      case 'reduce':
+        couch.sendLine([ true, reduce.reduce(line[1], line[2]) ]);
+        return;
+
+        break;
+
+      case 'rereduce':
+        couch.sendLine([ true, reduce.rereduce(line[1], line[2]) ]);
+        return;
+
+      default:
+        couch.log('Not implemented yet.');
+        couch.sendLine(false);
+        return;
+
+        break;
+    }
+
+    couch.sendLine(true);
+  });
 });
 
 process.stdin.resume();
